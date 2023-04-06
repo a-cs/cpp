23536aab9ca04b3720658bc328064185a701f6de
a-cs <acarneirosousa@gmail.com>
2023-04-05 23:09:08 -0300

    finishing ex05

--- cpp_01/ex05/Harl.cpp
+++ cpp_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: acarneir <acarneir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 21:30:06 by acarneir          #+#    #+#             */
-/*   Updated: 2023/04/05 21:39:51 by acarneir         ###   ########.fr       */
+/*   Updated: 2023/04/05 22:53:05 by acarneir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,24 @@ void Harl::debug( void ){
 	std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
 }
 
-void info( void ) {
+void Harl::info( void ) {
 	std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 
-void warning( void ) {
+void Harl::warning( void ) {
 	std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
 }
 
-void error( void ) {
+void Harl::error( void ) {
 	std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now.\n";
 }
 
-void	complain( std::string level ){
-	
+void	Harl::complain( std::string level ){
+	std::string	option[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*fp[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	for(int i=0; i < option->length(); i++){
+		if(option[i] == level)
+			return (this->*fp[i])();
+	}
+	return (this->*fp[3])();
 }
--- cpp_01/ex05/main.cpp
+++ cpp_01/ex05/main.cpp
@@ -6,14 +6,20 @@
 /*   By: acarneir <acarneir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 21:24:29 by acarneir          #+#    #+#             */
-/*   Updated: 2023/04/05 21:24:31 by acarneir         ###   ########.fr       */
+/*   Updated: 2023/04/05 22:52:56 by acarneir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
 #include <iostream>
 
 int	main(int argc, char **argv) {
-	std::cout << "entrou\n";
-	
+	Harl	harl;
+
+	if (argc !=2){
+		std::cout << "You should pass only 1 arguments.\nYou must choose between the following option:\nDEBUG, INFO, WARNING or ERROR.\n";
+		return 1;
+	}
+	harl.complain(argv[1]);
 	return (0);
 }
