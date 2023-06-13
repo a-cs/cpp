83ff38193e24cb7b31efbc100de76938ac27ae6a
a-cs <acarneirosousa@gmail.com>
2023-06-13 00:00:42 -0300

    finishing cpp05 ex02

---
+++ cpp_05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,21 @@
+#ifndef PRESIDENTIALPARDONFORM_H
+# define PRESIDENTIALPARDONFORM_H
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm: public AForm
+{
+	private:
+		std::string	target;
+	public:
+		PresidentialPardonForm();
+		PresidentialPardonForm(std::string target);
+		PresidentialPardonForm(PresidentialPardonForm const &obj);
+    	PresidentialPardonForm &operator=(PresidentialPardonForm const &obj);
+		virtual ~PresidentialPardonForm();
+
+		std::string		getTarget() const;
+		void	execute(Bureaucrat const &b) const;
+};
+
+#endif
--- cpp_05/ex02/main.cpp
+++ cpp_05/ex02/main.cpp
@@ -1,15 +1,18 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
 
 
 int	main( void ) {
 
-	Bureaucrat					john("John", 1);
+	Bureaucrat					john("John", 5);
 	Bureaucrat					mary("Mary", 150);
 	Bureaucrat					james("James", 11);
 	ShrubberyCreationForm		s("home");
 	RobotomyRequestForm			r("R2D2");
+	PresidentialPardonForm		p("C3PO");
 
 	try {
 		mary.executeForm(s);
@@ -23,6 +26,7 @@ int	main( void ) {
 	try {
 		james.signForm(s);
 		james.signForm(r);
+		james.signForm(p);
 	}
 	catch (std::exception & e) {
 		std::cout << e.what();
@@ -44,6 +48,13 @@ int	main( void ) {
 
 	try {
 		james.executeForm(r);
+	}
+	catch (std::exception & e) {
+		std::cout << e.what();
+	}
+
+	try {
+		john.executeForm(p);
 	}
 	catch (std::exception & e) {
 		std::cout << e.what();
