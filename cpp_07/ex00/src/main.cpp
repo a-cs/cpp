ebe90c9e8335ab3979665ffe6d644cd15d24092a
a-cs <acarneirosousa@gmail.com>
2023-07-04 00:15:40 -0300

    finishing cpp07 ex01

--- cpp_07/ex00/src/main.cpp
+++ cpp_07/ex00/src/main.cpp
@@ -1,23 +1,20 @@
 #include "../include/whatever.hpp"
 
 int	main() {
-	int a = 4;
-	int b = 2;
+	int a = 2;
+	int b = 3;
 
-	std::cout << "---------Testing with ints---------" << std::endl;
-	std::cout << "a = " << a << " and b = " << b << std::endl;
-	::swap(a,b);
-	std::cout << "a = " << a << " and b = " << b << std::endl;
-	std::cout << "min("<< a <<", " << b << ") = " << ::min(a, b) << std::endl;
-	std::cout << "max("<< a <<", " << b << ") = " << ::max(a, b) << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::string str1 = "abc";
-	std::string str2 = "xyz";
-	std::cout << "\n\n---------Testing with strings---------" << std::endl;
-	std::cout << "str1 = " << str1 << " and str2 = " << str2 << std::endl;
-	::swap(str1,str2);
-	std::cout << "str1 = " << str1 << " and str2 = " << str2 << std::endl;
-	std::cout << "min('"<< str1 <<"', '" << str2 << "') = " << ::min(str1, str2) << std::endl;
-	std::cout << "max('"<< str1 <<"', '" << str2 << "') = " << ::max(str1, str2) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
---
+++ cpp_07/ex01/src/main.cpp
@@ -0,0 +1,14 @@
+#include "../include/iter.hpp"
+
+int	main() {
+	int		intArr[4]  = { 4, 2, 1, 3};
+	char	charArr[3]  = { 'a', 'b', 'c'};
+
+	std::cout << "---------Testing with int array---------" << std::endl;
+	::iter(intArr, 4, ::print);
+
+	std::cout << "\n---------Testing with char array---------" << std::endl;
+	::iter(charArr, 3, ::print);
+
+	return 0;
+}
