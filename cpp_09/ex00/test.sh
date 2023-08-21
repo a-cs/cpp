4e1be7bd4f55c89278ce8bb977b61a463eebc5a3
a-cs <acarneirosousa@gmail.com>
2023-08-21 11:28:16 -0300

    updating cpp09 ex00

--- cpp_09/ex00/test.sh
+++ cpp_09/ex00/test.sh
@@ -3,14 +3,14 @@ make && clear
 NAME=btc
 
 echo "\e[1;32mTest 1: Valid file name\e[0m"
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --tool=memcheck -q ./$NAME input.txt
+./$NAME input.txt
 echo
 
-echo "\e[1;31mTest 2:Invalid file name\e[0m"
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --tool=memcheck -q ./$NAME invalidFilename.txt
+echo "\e[1;31mTest 2: Invalid file name\e[0m"
+./$NAME invalidFilename.txt
 echo
 
 
-echo "\e[1;31mTest 2:Invalid values\e[0m"
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --tool=memcheck -q ./$NAME invalidValues.txt
+echo "\e[1;31mTest 3: Invalid values\e[0m"
+./$NAME invalidValues.txt
 echo
